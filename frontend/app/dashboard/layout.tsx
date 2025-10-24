ef09cee8c044c3f3ff63f4c141d55e4bc8225e4f
Sathyanarayanansakthi <sathyanarayanansakthi@gmail.com>
2025-10-24 11:32:39 +0530

    Changes Done

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -1,13 +1,13 @@
 // app/dashboard/layout.tsx
 import { ReactNode } from "react"
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/app/dashboard/components/sidebar/sidebar"
+import { AppSidebar } from "@/components/Sidebar"
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
     <SidebarProvider>
       <div className="flex min-h-screen">
-        <AppSidebar />
+        <AppSidebar/>
         <main className="flex-1 p-4">
           <SidebarTrigger />
           {children}
